c48bf0def2786728fb148b909470fc78753ffeb7
Shreyas Raman <shreyassraman@gmail.com>
2021-12-20 10:47:12 +0400

    final WSDDN code

--- object_detection_networks/WSDDN/roi_pooling.py
+++ object_detection_networks/WSDDN/roi_pooling.py
@@ -9,7 +9,7 @@ def ROI_Pooling(feature_map):
 
     '''Normalizes the input feature map to a fixed output shape'''
 
-    roi_pooling = tfa.layers.AdaptiveMaxPooling2D(output_size = (hp.roi_pooling_output[0], hp.roi_pooling_ouput[1]))
+    roi_pooling = tfa.layers.AdaptiveMaxPooling2D(output_size = (hp.roi_pooling_output[0], hp.roi_pooling_output[1]))
 
     #apply pooling to the base feature maps from VGG network
     pooling_output = roi_pooling(feature_map)
