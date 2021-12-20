166b1c810078a66182baf2dee1f6ea5160af4012
Shreyas Raman <shreyassraman@gmail.com>
2021-12-21 01:03:29 +0400

    final WSDDN code

--- object_detection_networks/WSDDN/run.py
+++ object_detection_networks/WSDDN/run.py
@@ -202,8 +202,10 @@ def train(model, train_data, val_data, checkpoint_path, logs_path):
 
                 loss_value = loss_value + model.crossentropy_loss(tf.expand_dims(output, axis=0), tf.cast(label, tf.float32)) + model.l2_regularizer() + spatial_regularizer_output
 
+
+            loss_value = loss_value/len(image_batch) #loss averaged over batch
+
         pdb.set_trace()
-        loss_value = loss_value/len(image_batch) #loss averaged over batch
         grads = tape.gradient(loss_value, model.trainable_weights)
 
         #set the learning rate using the number of iterations for optimizer
