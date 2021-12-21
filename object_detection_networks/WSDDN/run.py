24ccd601464078e41455c3474c1b281d040ef2db
Shreyas Raman <shreyassraman@gmail.com>
2021-12-21 21:42:47 +0400

    final WSDDN code

--- object_detection_networks/WSDDN/run.py
+++ object_detection_networks/WSDDN/run.py
@@ -177,7 +177,7 @@ def parse_arguments():
 def drawBoxes(boxes):
     pdb.set_trace()
     for (x, y, w, h) in boxes:
-        random_color = np.random.random(size=(3,))
+        random_color = [1,0,0]
         plt.hlines(y, x, x + w, colors= random_color)
         plt.hlines(y + h, x, x + w, colors= random_color)
         plt.vlines(x, y, y + h, colors= random_color)
